ea64b133386816e31ef14af06ef9b6a9f1b8bf9f
Chris Delong <admin@frosthaven.net>
2022-01-07 15:41:00 -0700

    Created Spring Boot Project

---
+++ GamesDB/src/main/java/com/skilldistillery/games/GamesDbApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesDbApplication.class, args);
+	}
+
+}
